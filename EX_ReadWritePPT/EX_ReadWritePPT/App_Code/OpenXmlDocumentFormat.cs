6224d1b07d9ebdfacf89fb2140f6285fbb65429c
efrainmejiasc <efrainmejiasc@hotmail.com>
2020-10-01 16:53:36 -0400

    update

---
+++ EX_ReadWritePPT/EX_ReadWritePPT/App_Code/OpenXmlDocumentFormat.cs
@@ -0,0 +1,35 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de OpenXmlFormat
+/// </summary>
+public class OpenXmlDocumentFormat
+{
+    public OpenXmlDocumentFormat()
+    {
+    }
+
+    public bool WriteOnSlide()
+    {
+        string filePath = System.Web.HttpContext.Current.Server.MapPath("~/TemplatePPT/Plantilla1.pptx");
+        try
+        {
+            using (PresentationDocument presentationDocument = PresentationDocument.Open(filePath, true))
+            {
+                PresentationPart presentationPart = presentationDocument.PresentationPart;
+                Presentation presentation = presentationPart.Presentation;
+            }
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
--- EX_ReadWritePPT/EX_ReadWritePPT/Default.aspx.cs
+++ EX_ReadWritePPT/EX_ReadWritePPT/Default.aspx.cs
@@ -13,10 +13,15 @@ public partial class _Default : Page
     protected void Page_Load(object sender, EventArgs e)
     {
 
-        EscribirUsandoOffice15();
+        //EscribirUsandoOffice15();
+        OpenXmlExample();
     }
 
-
+    private void OpenXmlExample()
+    {
+        OpenXmlDocumentFormat example = new OpenXmlDocumentFormat();
+        example.WriteOnSlide();
+    }
 
     private void EscribirUsandoOffice15()
     {
